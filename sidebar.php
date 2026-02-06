5b201054af670056446d4cc45e0748a9550d117c
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-06 15:01:21 +0530

    Changed

--- sidebar.php
+++ sidebar.php
@@ -463,7 +463,7 @@ $userInitial = strtoupper(substr($userEmail, 0, 1));
                 </div>
                 <div class="logo-text">
                     <span class="logo-title">St. Xavier's</span>
-                    <span class="logo-subtitle">Mail System</span>
+                    <!-- <span class="logo-subtitle">Mail System</span> -->
                 </div>
             </a>
             <button class="toggle-sidebar" id="toggleSidebar" aria-label="Toggle Sidebar">
