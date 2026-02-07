b0894dfde096cbf7415a98aeab175d0b24ea5e3b
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-07 19:01:34 +0530

    Changed

--- index.php
+++ index.php
@@ -164,6 +164,7 @@ $defaultSignature = json_encode($settings['signature']);
         /* Form sections - Nature.com article sections */
         .form-section {
             margin-bottom: 48px;
+            background-color:gery;
         }
 
         .section-title {
