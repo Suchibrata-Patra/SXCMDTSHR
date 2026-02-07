d5ffab10eea90c96f853c7a321ca73ba15e30cd1
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-07 16:48:26 +0530

    Changed

--- sidebar.php
+++ sidebar.php
@@ -14,9 +14,7 @@ $unlabeledCount = getUnlabeledEmailCount($userEmail);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Harding:wght@400;500;600;700&family=Inter:wght@400;500;600;700&family=Merriweather:wght@300;400;700&display=swap"
-        rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
     <style>
         :root {
