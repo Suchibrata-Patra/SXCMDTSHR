fdd410ce876d2acd297b07630026419898e66042
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-07 16:49:32 +0530

    Chanegd

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $defaultSignature = json_encode($settings['signature']);
     <!-- Google Fonts - Nature.com uses Harding and Inter -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Harding:wght@400;500;600&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Harding:wght@400;500;600&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet"> -->
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     
