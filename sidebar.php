6d3d55467f3e75c7fc4d85a82698ca47e988f737
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-07 17:14:25 +0530

    Changed

--- sidebar.php
+++ sidebar.php
@@ -311,7 +311,7 @@ $unlabeledCount = getUnlabeledEmailCount($userEmail);
 
         <div class="user-footer">
             <div class="user-card">
-                <span class="auth-badge"><img src="SXCMDTSHR/Assets/image/Verified_badge.png" alt=""></span>
+                <span class="auth-badge"><img src="/Assets/image/Verified_badge.png" alt=""></span>
                 <!-- <span class="auth-badge">Verified Account</span> -->
                 <span class="user-email"><?= htmlspecialchars($userEmail) ?></span>
             </div>
