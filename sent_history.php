309a1f785ef45084ae02624b332ac20577d812ac
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-10 22:55:09 +0530

    Changed

--- sent_history.php
+++ sent_history.php
@@ -1146,7 +1146,7 @@ $labels = getLabelCountsForSent($userEmail) ?? [];
                     <div class="search-box">
                         <span class="material-icons">search</span>
                         <input type="text" id="searchInput" placeholder="Search sent emails..." oninput="searchMessages()">
-                        <span class="material-icons clear-search" id="clearSearch" onclick="clearSearch()"></span>
+                        <span class="material-icons clear-search" id="clearSearch" onclick="clearSearch()">close</span>
                     </div>
                     <div class="filter-group">
                         <select id="labelFilter" class="filter-select" onchange="filterByLabel()">
