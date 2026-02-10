497ea495a88ab7d09705d8a4f33a83e264575bea
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-10 23:23:21 +0530

    Changed

--- sent_history.php
+++ sent_history.php
@@ -487,7 +487,7 @@ $labels = getLabelCountsForSent($userEmail) ?? [];
         .stat-icon {
             width: 32px;
             height: 32px;
-            border-radius: 8px;
+            border-radius: 50px;
             display: flex;
             align-items: center;
             justify-content: center;
