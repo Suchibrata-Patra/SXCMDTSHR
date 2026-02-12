3f5c6f7468671e0e090bdc49e9e7cfffe3127899
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-12 13:40:30 +0530

    Chanegd

--- sidebar.php
+++ sidebar.php
@@ -411,6 +411,10 @@ $unlabeledCount = getUnlabeledEmailCount($userEmail);
             <span class="material-icons">mail</span>
             <span>Inbox</span>
         </a>
+         <a href="inbox.php" class="nav-item <?= ($current_page == 'inbox') ? 'active' : ''; ?>">
+            <span class="material-icons">mail</span>
+            <span>Inbox</span>
+        </a>
         <a href="drive.php" class="nav-item <?= ($current_page == 'drive') ? 'active' : ''; ?>">
             <span class="material-icons">drive_export</span>
             <span>Drive</span>
