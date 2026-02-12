d10aab8f63a1425351132008023abcd23563a504
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-12 23:39:34 +0530

    Chanegd

--- login.php
+++ login.php
@@ -511,7 +511,7 @@ function authenticateWithSMTP($email, $password) {
                 <!-- St. Xavier's College (Autonomous), Kolkata<br>
                 Mail Delivery & Tracking System v2.0 -->
                 <br>
-                <span style="font-size:12px;">Made with ♥︎ by SXC MDTS Students</span>
+                <span style="font-size:20px;">Made with ♥︎ by SXC MDTS Students</span>
             </footer>
         </div>
     </div>
