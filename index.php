27d346de4f5fb22832125d651605786d69f3f4c6
Suchibrata Patra <101924210+Suchibrata-Patra@users.noreply.github.com>
2026-02-12 12:21:32 +0530

    Changed

--- index.php
+++ index.php
@@ -41,8 +41,10 @@ $_SESSION['user_settings'] = $settings;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php iclude (favicon.php) ?>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <?php
+        define('PAGE_TITLE', 'SXC MDTS | Dashboard');
+        include 'favicon.php';
+    ?>    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
